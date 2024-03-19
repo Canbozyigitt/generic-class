dc7659b1b5373898e5855a42bcaae0392b5c3383
Canbozyigitt <136346490+Canbozyigitt@users.noreply.github.com>
2024-03-19 18:07:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Integer a =10;
+        Integer a =4;
         String b="Can";
         Double d=3.14;
         Test<String,Integer,Double> t=new Test<>(b,a,d);
